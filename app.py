586d3a9cc5ad275fcf0240966025ad54dfa4308c
thanktua-spp <egbethankgodjnr@gmail.com>
2023-12-01 07:37:54 +0100

    stable streamlit app with no models
    Former-commit-id: 1a391062f0b045c6a1d6434d646bcd8e2b4bf08a

--- app.py
+++ app.py
@@ -9,28 +9,38 @@ def make_prediction(age, scholarship, hipertension, diabetes, alcoholism, sms_re
     prediction = "Your prediction"  # Replace this with your prediction logic
     return prediction
 
-st.sidebar.title("Patient Information")
-age = st.sidebar.slider('Age', 1, 150, 27, 1)
-gender = st.sidebar.selectbox('Gender', ["Male", "Female"], index=0)
-number_missed = st.sidebar.slider('Number of appointment missed', 0, 10, 0, 1)
 
-hipertension, diabetes, alcoholism, handicap, sms_received, scholarship = st.sidebar.columns(6)
-with hipertension:
-    hipertension = st.radio('Hypertension', ["Yes", "No"])
-with diabetes:
-    diabetes = st.radio('Diabetes', ["Yes", "No"])
-with alcoholism:
-    alcoholism = st.radio('Alcoholism', ["Yes", "No"])
-with handicap:
-    handicap = st.radio('Disability', ["Yes", "No"])
-with sms_received:
-    sms_received = st.radio('SMS', ["Yes", "No"])
-with scholarship:
-    scholarship = st.radio('Scholarship', ["Yes", "No"])
+def main():
+    st.title('No Shows Application Demo Interface')
+    st.write('Hello, welcome! This is an interactive demo interface were you can make real time predictions using several trained models')
+    
+    st.title("Patient Information")
+    age = st.slider('Age', 1, 150, 27, 1)
+    gender = st.radio('Gender', ["Male", "Female"], index=0)
+    number_missed = st.slider('Number of appointment missed', 0, 10, 0, 1)
 
-awaiting_time = st.sidebar.slider('Awaiting time in days', 0, 15, 0, 1)
-selected_model = st.sidebar.selectbox('Model', ["Logistic", "DecisionTree", "RandomForest", "XGBoost", "MLP", "SVC"])
+    hipertension, diabetes, alcoholism, handicap, sms_received, scholarship = st.columns(6)
+    with hipertension:
+        hipertension = st.radio('Hypertension', ["Yes", "No"])
+    with diabetes:
+        diabetes = st.radio('Diabetes', ["Yes", "No"])
+    with alcoholism:
+        alcoholism = st.radio('Alcoholism', ["Yes", "No"])
+    with handicap:
+        handicap = st.radio('Disability', ["Yes", "No"])
+    with sms_received:
+        sms_received = st.radio('SMS', ["Yes", "No"])
+    with scholarship:
+        scholarship = st.radio('Scholarship', ["Yes", "No"])
 
-if st.sidebar.button('Prediction'):
-    prediction = make_prediction(age, scholarship, hipertension, diabetes, alcoholism, sms_received, awaiting_time, number_missed, handicap, gender, selected_model)
-    st.write(f"Prediction: {prediction}")
+    awaiting_time = st.slider('Awaiting time in days', 0, 15, 0, 1)
+    selected_model = st.radio('Model', ["Logistic", "DecisionTree", "RandomForest", "XGBoost", "MLP", "SVC"])
+
+    if st.button('Prediction'):
+        prediction = make_prediction(age, scholarship, hipertension, diabetes, alcoholism, sms_received, awaiting_time, number_missed, handicap, gender, selected_model)
+        st.write(f"Prediction: {prediction}")
+
+
+
+if __name__ == '__main__':
+    main()
