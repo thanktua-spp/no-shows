71956092683e0dec158f1aa68d5b1a4220fbab00
thanktua-spp <egbethankgodjnr@gmail.com>
2023-12-03 00:47:51 +0100

    updated all model files
    Former-commit-id: 58e5d31d69ec5bbb225a5f1e57a496f03aa96229

--- app.py
+++ app.py
@@ -1,13 +1,68 @@
 from joblib import load
 import streamlit as st
+import pickle
 
 
+# Load the model
+svc_model = pickle.load(open('models/SVC.sav', "rb"))
+decision_tree_model = pickle.load(open('models/DecisionTreeClassifier.sav', "rb"))
+randomforest_model = pickle.load(open('models/RandomForestClassifier.sav', "rb"))
+logistic_regression_model = pickle.load(open('models/LogisticRegression.sav', "rb"))
+xgb_model = pickle.load(open('models/xgb.XGBClassifier.sav', "rb"))
+mlp_model = pickle.load(open('models/MLPClassifier.sav', "rb"))
+
+def hot_encode_inputs(inputs):
+    headers = [
+        'Age', 'AwaitingTime', 'Number_Missed', 'SMS_received_0', 'SMS_received_1',
+        'Scholarship_0', 'Scholarship_1', 'Handcap_0', 'Handcap_1', 'Gender_F', 'Gender_M',
+        'Hipertension_0', 'Hipertension_1', 'Diabetes_0', 'Diabetes_1', 'Alcoholism_0', 'Alcoholism_1'
+    ]
+    
+    encoded_inputs = []
+    for item in inputs:
+        if item == 'Yes':
+            encoded_inputs.append(1)
+        elif item == 'No':
+            encoded_inputs.append(0)
+        elif item == 'Male':
+            encoded_inputs.extend([0, 1])  # Male: [0, 1]
+        elif item == 'Female':
+            encoded_inputs.extend([1, 0])  # Female: [1, 0]
+        else:
+            encoded_inputs.append(item)
+    
+    # Fill in any missing values with 0
+    while len(encoded_inputs) < len(headers):
+        encoded_inputs.append(0)
+    
+    return encoded_inputs
+
+    
 
 def make_prediction(age, scholarship, hipertension, diabetes, alcoholism, sms_received, awaiting_time, number_missed, handicap, gender, selected_model):
-    st.write(f"Inputs: {age}, {gender}, {number_missed}, {hipertension}, {diabetes}, {alcoholism}, {handicap}, {sms_received}, {awaiting_time}, {selected_model}")
+    st.write(f"Inputs: {age}, {awaiting_time}, {number_missed}, {sms_received}, {scholarship}, {handicap}, {gender}, {diabetes}, {alcoholism}, {selected_model}")
     # Perform model prediction here based on selected_model and input values
-    prediction = "Your prediction"  # Replace this with your prediction logic
-    return prediction
+    if gender == 'Male': gender = 1
+    else: gender = 0
+
+    parameters = [age, awaiting_time, number_missed, sms_received, scholarship, handicap, gender, hipertension, diabetes, alcoholism]
+    parameters_enc = hot_encode_inputs(parameters)
+    
+    if selected_model == "Logistic":
+        prediction = logistic_regression_model.predict([parameters_enc])[0]
+    elif selected_model == "DecisionTree":
+        prediction = decision_tree_model.predict([parameters_enc])[0]
+    elif selected_model == "XGBoost":
+        prediction = xgb_model.predict([parameters_enc])[0]
+    elif selected_model == "RandomForest":
+        prediction = randomforest_model.predict([parameters_enc])[0]
+    elif selected_model == "SVC":
+        prediction = svc_model.predict([parameters_enc])[0]
+    elif selected_model == "MLP":
+        prediction = mlp_model.predict([parameters_enc])[0]     
+          
+    if prediction == 1: return "No shows"
+    else: return "Shows"
 
 
 def main():
